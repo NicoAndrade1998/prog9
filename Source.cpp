2c9b419c4e07871bac0a1ac63e69ad75a3b10d00
Nico Andrade <nicoandrade1998@gmail.com>
2022-12-06 15:04:18 -0800

    first commit

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv){
+    ifstream cities("city.txt");
+    ifstream roads("road.txt");
+
+
+
+    return 0;
+}

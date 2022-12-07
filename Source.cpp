2740015b653bcdd23696d305c26fbfa5366aed5f
NicoAndrade1998 <89725547+NicoAndrade1998@users.noreply.github.com>
2022-12-06 22:39:13 -0800

    Added parse_city and print_city from yiquan.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,9 @@ using namespace std;
 #include <string>
 #include <fstream>
 #include <limits.h>
-
-struct city{
-    int ID;
-    string City_Code;
-    string City_Code;
-    int population;
-    int elevation;
-};
-
-struct road{
-    int From_City;
-    int To_City;
-    int distance;
-};
-
+#include <vector>
+#include <unordered_map>
+#include <sstream>
 #define V 20 //the amount of cities
 
 // A utility function to find the vertex with minimum
@@ -67,7 +55,7 @@ void dijkstra(int graph[V][V], int src, int goal)
         // Mark the picked vertex as processed
         sptSet[u] = true;
 
-        int *visited=new int[V]=0;
+        int *visited= new int[V];
         // Update dist value of the adjacent vertices of the
         // picked vertex.
         for (int v = 0; v < V; v++)
@@ -92,7 +80,7 @@ void dijkstra(int graph[V][V], int src, int goal)
     if(dist[goal])
     {
       //print statement of cities called for
-      for(int i=0;i<visits<i++)
+      for(int i=0;i<visits; i++)
       {
         //call to a method to print the names of the visited cities;
       }
@@ -103,15 +91,84 @@ void dijkstra(int graph[V][V], int src, int goal)
     }
 }
 
+
+class city {
+public:
+    int id;
+    string city_code;
+    string city_name;
+    int population;
+    int elevation;
+
+    city(vector<string> tokens) {
+        id = stoi(tokens[0]);
+        city_code = tokens[1];
+        city_name = tokens[2];
+        population = stoi(tokens[3]);
+        elevation = stoi(tokens[4]);
+    }
+};
+
+void parse_city(ifstream &f_cities, unordered_map<string, city> &cities) {
+    string line;
+    while (getline(f_cities, line)) {
+        vector<string> tokens;
+        istringstream iss(line);
+        string token;
+
+        while (getline(iss, token, '\t')) {
+        tokens.push_back(token);
+        }
+
+        city tmp(tokens);
+        cities.insert({tmp.city_code, tmp});
+    }
+}
+
+void print_city(unordered_map<string, city> &cities, string city) {
+    cout << "city code: " << city << endl;
+    auto tmp = cities.at(city);
+    cout << tmp.id << " ";
+    cout << tmp.city_code << " ";
+    cout << tmp.city_name << " ";
+    cout << tmp.population << " ";
+    cout << tmp.elevation << " " << endl;
+}
+
 int main(int argc, char** argv){
-    //parsing user input
-    char* From_City = argv[0];
-    char* To_City = argv[1];
+    string from_city = argv[1];
+    string to_city = argv[2];
+    //string from_city = "AN";
+    //string to_city = "BK";
+
+    ifstream f_cities("city.txt");
+    ifstream f_roads("road.txt");
+
+    unordered_map<string, city> cities;
+    parse_city(f_cities, cities);
+
+    print_city(cities, from_city);
+    print_city(cities, to_city);
+
+    
+    //creating adjacency list
+    int adj_list[20][20];
+    int i, x, y; //x and y are indexes, i is input
+    while(!f_roads.eof()){
+      f_roads >> i;
+      f_roads >> x;
+      f_roads >> y;
+      //cout << i << " "<< x << " " << y << endl; //for testing
+      adj_list[x][y] = i;
+    }
+
+    //convert input code to ID and calls dijkstra
+    int to, from;
+    auto tmp1 = cities.at(from_city);
+    auto tmp2 = cities.at(to_city);
+    dijkstra(adj_list, tmp1.id, tmp2.id);
 
 
-    //reading input files
-    ifstream cities("city.txt");
-    ifstream roads("road.txt");
 
 
     return 0;
